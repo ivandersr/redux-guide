287d3436dc82c69ea3bf5570bdbdcfc8c7ee8bd9
Ivander <ivander.ruiz@dotgroup.com.br>
2023-07-10 16:05:47 -0300

    feat(redux): added user slice with toolkit

--- src/redux/cart/action-types.js
+++
@@ -1,8 +0,0 @@
-const CartActionTypes = {
-  ADD_PRODUCT: 'cart/addProduct',
-  REMOVE_PRODUCT: 'cart/removeProduct',
-  INCREMENT_PRODUCT_COUNT: 'cart/incrementProductCount',
-  DECREMENT_PRODUCT_COUNT: 'cart/decrementProductCount'
-};
-
-export default CartActionTypes;
---
+++ src/redux/user/slice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  currentUser: null
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loginUser: (state, action) => {
+      state.currentUser = action.payload;
+    },
+    logoutUser: (state, action) => {
+      state.currentUser = null;
+    }
+  }
+});
+
+export const {
+  loginUser,
+  logoutUser
+} = userSlice.actions;
+
+export default userSlice.reducer;
